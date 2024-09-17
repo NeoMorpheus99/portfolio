e529778c4b4e1dc00f05d08196cafde8840009c6
norbuTLepcha <lepcha296@gmail.com>
2024-09-17 23:52:28 +0600

    update

---
+++ src/ProjectTiles.jsx
@@ -0,0 +1,20 @@
+import profileData from "./ProfileData";
+
+const ProjectCard = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col">
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <p>Tis is a sampple</p>
+          </div>
+        </div>
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectCard;
